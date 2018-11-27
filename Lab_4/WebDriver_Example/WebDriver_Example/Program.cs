b1db07334472bd47dc9b9320e5f602c5e6b352c9
Ivan Shaveka <shaveko.ivan@gmail.com>
2018-11-27 21:01:31 +0300

    Lab_4

---
+++ Lab_4/WebDriver_Example/WebDriver_Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriver_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.klm.com/home/by/ru";
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts();
+
+            
+            driver.FindElement(By.ClassName("g-locationpicker--origin")).SendKeys("Minsk - Minsk National Airport (MSQ), Беларусь");
+            driver.FindElement(By.ClassName("g-locationpicker--destination")).SendKeys("Пулман - Moscow Regional (PUW), Washington - США");
+
+            driver.FindElement(By.ClassName("g-forms-datepicker")).Click();
+            driver.FindElement(By.XPath("//*[@ui-state-default ui-state-active='28']")).Click();
+            driver.FindElement(By.ClassName("g-btn g-btn-primary g-search-form--footer-continue-button")).Click();
+
+        }
+    }
+}
